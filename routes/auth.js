4b209fc026a515ed22fdc3e7fa8cfbd8e746adb7
aissatou00 <aissatousalla2709@gmail.com>
2024-11-26 22:49:30 +0100

    modif

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/protected', authMiddleware, (req, res) => {
-    res.status(200).json({ message: 'Vous êtes autorisé.', user: req.user });
+    res.status(200).json({ message: 'Vous êtes autorisés.', user: req.user });
 });
 
 module.exports = router ;
